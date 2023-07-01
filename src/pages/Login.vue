2ba07c5bc61718dc9695550343b6ec1e90f86403
Muhammad Taqiyuddin <mtaqiyuddin101@gmail.com>
2023-07-02 02:19:33 +0700

    1

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -176,7 +176,7 @@
                   type="submit"
                   class="flex w-full justify-center rounded-full bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Register
+                  Login
                 </button>
               </div>
             </form>
